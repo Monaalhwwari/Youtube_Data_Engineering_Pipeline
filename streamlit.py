b72532d872ea2fbe2bfee7a380cd0a359a8417fc
Mona_alhwwari <monaalhwwari@outlook.com>
2026-05-17 16:00:12 +0300

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,30 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
 import streamlit as st
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("postgresql://postgres:password@localhost:5432/Youtube_API")
+load_dotenv()
+
+password = os.getenv("db_password")
+
+connection_url = URL.create(
+    "postgresql+psycopg2",
+    username="postgres",
+    password=password,
+    host="127.0.0.1",
+    port=5432,
+    database="Youtube_API"
+)
+
+engine = create_engine(connection_url)
 
 df = pd.read_sql("SELECT * FROM youtube_videos", engine)
 
-
+# visualaization
 
 st.title("YouTube Dashboard")
 
-# Kpi
 views,likes,videos=st.columns(3)
 
 views.metric("Total Views", df["viewCount"].sum())
@@ -35,7 +49,7 @@ time_df = df.groupby("Date")[["viewCount", "likeCount"]].sum()
 
 st.line_chart(time_df)
 
-# choose channel to view their stats
+# choose channel
 st.subheader("Choose a channel to view their stats")
 channel = st.selectbox("Select Channel", df["channelTitle"].unique())
 filtered_df = df[df["channelTitle"]==channel]
